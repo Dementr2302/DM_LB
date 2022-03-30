045b66626c37f5a2cdd40e34873fe032679c86e5
Dementr2302 <94387603+Dementr2302@users.noreply.github.com>
2022-03-30 10:14:20 +0300

    First try
    1

---
+++ Num_11/Program.fs
@@ -0,0 +1,23 @@
+﻿// LB_5_NUM_11
+
+
+
+open System
+
+
+
+
+let test w =
+    match w with
+    |"F#"->printf " ТЫ подлиза "
+    |"Prolog"->printf " ТЫ подлиза "
+    |_->printf"Что это"
+
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "Какой твой любимый язык программирования?"
+    let w = System.Console.ReadLine();
+    test w
+    0
