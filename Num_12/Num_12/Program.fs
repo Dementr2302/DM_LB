72298ce3699c0ede47c5fa53489d4a4aaedb4c6c
Dementr2302 <94387603+Dementr2302@users.noreply.github.com>
2022-04-05 13:33:21 +0300

    Add files via upload

---
+++ Num_12/Num_12/Program.fs
@@ -0,0 +1,11 @@
+﻿// LB_5_NUM_12
+open System
+
+[<EntryPoint>]
+let main argv=
+    printf "Той любимый ЯП?"
+    let f1 (x:string) = printfn "%s" x
+    let f2 (x:string) : string = if (x="Prolog") || (x="F#") then "Подлиза" else "Что это?"
+    let w = Console.ReadLine()
+    (f2>>f1) w
+    0
