228c7192ac9e301f6dcf12c07f6f56f63e0a30ad
Dementr2302 <94387603+Dementr2302@users.noreply.github.com>
2022-04-08 09:54:01 +0300

    Add files via upload

---
+++ Num_14/Program.fs
@@ -0,0 +1,21 @@
+﻿//Num_14
+
+open System
+
+let del n f init =
+    let rec delnew n w init dev=
+       if (dev=0) then init
+       else
+            let newinit = if (n % dev = 0)
+                            then w init dev
+                          else init
+            let newdev = dev - 1
+            delnew n w newinit newdev
+    delnew n f init n
+
+    
+let vod =
+    printf"Введите число : "
+    let n = Console.ReadLine()|>Int32.Parse
+    del n (fun x y-> x+y) 0 |> Console.WriteLine
+    0
