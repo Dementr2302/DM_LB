fad8ee46487b665d3eeeba80fb0d1ab869837fd4
Dementr2302 <94387603+Dementr2302@users.noreply.github.com>
2022-04-10 13:47:04 +0300

    Add files via upload

---
+++ Num_15/Program.fs
@@ -0,0 +1,27 @@
+﻿//Num_15
+
+open System
+//Взаимно простые числа — целые числа, не имеющие никаких общих делителей, кроме ±1
+//Два целых числа a и b называются взаимно простыми, если их наибольший общий делитель равен единице
+
+
+let rec del a b =
+    if a=b then a
+    else 
+        if a>b then del (a-b) b 
+        else del a (b-a) 
+ 
+let OB x F init =
+    let rec OB' x F init chislo =
+        if chislo=0 then init
+        else 
+            let new_init = if (del x chislo) =1 then F init chislo else init
+            let new_chislo= chislo - 1
+            OB' x F new_init new_chislo
+    OB' x F init x
+ 
+let vod =
+    let x = System.Convert.ToInt32(System.Console.ReadLine())
+    System.Console.WriteLine(OB x (fun x y -> x+y) 0)
+    0
+   
