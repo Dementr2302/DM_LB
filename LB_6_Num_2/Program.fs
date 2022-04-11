24c1e9ae6cf7300e5e4a97e5b6289e2e5a125b63
Dementr2302 <94387603+Dementr2302@users.noreply.github.com>
2022-04-11 18:44:04 +0300

    Add files via upload

---
+++ LB_6_Num_2/Program.fs
@@ -0,0 +1,31 @@
+﻿// min index 
+open System
+
+let rec rList n = 
+    if n=0 then []
+    else
+    let Head = System.Convert.ToInt32(System.Console.ReadLine())
+    let Tail = rList (n-1)
+    Head::Tail
+
+let rData = 
+    Console.WriteLine("Введите размерность списка:  ")
+    let n=System.Convert.ToInt32(System.Console.ReadLine())
+    rList n
+
+
+let MinIndex list = 
+    let rec minel list min indM indEL =
+        match list with
+        |[]->indM
+        |h::tail -> 
+            let newMin =if h<min then h else min
+            let newInd = if h<min then indEL else indM
+            minel tail newMin newInd (indEL+1)
+    minel list list.Head 0 0
+
+let vod =
+    let n = rData
+    Console.WriteLine("Индекс минимума: ")
+    Console.WriteLine(MinIndex n)
+    0
