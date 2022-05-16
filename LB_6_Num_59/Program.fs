68e70bad8c701d79ecc88eabe39c917e68b7f8c0
Dementr2302 <94387603+Dementr2302@users.noreply.github.com>
2022-05-16 18:44:59 +0300

    Add files via upload

---
+++ LB_6_Num_59/Program.fs
@@ -0,0 +1,65 @@
+﻿open System
+
+let rec readList n = 
+    if n=0 then []
+    else
+    let Head = System.Convert.ToInt32(System.Console.ReadLine())
+    let Tail = readList (n-1)
+    Head::Tail
+
+let readData = 
+    Console.WriteLine("Введите размерность списка:  ")
+    let n=System.Convert.ToInt32(System.Console.ReadLine())
+    Console.WriteLine("Введите список: ")
+    readList n
+
+let rec writeList = function
+    [] ->   let z = System.Console.ReadKey()
+            0
+    | (head : int)::tail -> 
+                       System.Console.WriteLine(head)
+                       writeList tail 
+
+
+
+
+
+let chst elem list= 
+    let rec count list el num = 
+        match list with 
+        |[]->num
+        |h::tail->
+            let newNum = if el=h then (num+1)else num
+            count tail el newNum
+    count list elem 0
+ 
+
+let dunc predicate f list  =
+    let rec filt list pr newList = 
+        match list with
+        |[]->newList
+        |h::tail ->
+            let nextNewList = 
+                if pr h then (newList @ [f h])
+                else newList
+            filt tail pr nextNewList
+    filt list predicate []
+
+
+let del list el = dunc (fun x -> (x <> el)) (fun x->x) list 
+let uniq list = 
+    let rec uniq1 list newList = 
+        match list with
+            |[] -> newList
+            | h::t -> 
+                        let listWithout = del t h
+                        let newnewList = newList @ [h]
+                        uniq1 listWithout newnewList
+    uniq1 list [] 
+
+[<EntryPoint>]
+let main argv =
+    let l = readData
+    Console.WriteLine("Получившийся список:")
+    filter (fun x -> x<100 && x>0 && (chst x l)>2) (fun x -> x*x) l|>uniq |>writeList
+    0 
