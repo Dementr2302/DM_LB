89e7079fb4cd14560b96250d6986e91f178c00dd
Dementr2302 <94387603+Dementr2302@users.noreply.github.com>
2022-05-17 19:27:34 +0300

    1-3

--- Num_18/Program.fs
+++ Num_18/Program.fs
@@ -1,54 +1,69 @@
-﻿open System
 
+open System
 
-//1
-
-
-let Sum n = 
-    let rec pro n div = 
+//Метод 1 - найти сумму простых делителей числа 
+let Pro n = 
+    let rec Pro1 n div = 
         if div = 1 then true
         else 
             if n%div=0 then false
             else 
                 let newDiv=div-1
-                pro n newDiv
-    pro n (n-1)
+                Pro1 n newDiv
+    Pro1 n (n-1)
 
-let sum' n = 
-    let rec sump n sumInit div = 
+let Sum n = 
+    let rec Sum1 n sumInit div = 
         if div = 1 then sumInit
         else 
             let NextSum=
-                if n%div=0 && (Sum div = true) then sumInit+div
+                if n%div=0 && (Pro div = true) then sumInit+div
                 else sumInit
             let newDiv=div-1
-            sump n NextSum newDiv
-    sump n 0 n  
+            Sum1 n NextSum newDiv
+    Sum1 n 0 n  
 
-
-
-    //2
-
-
-let num n = 
-    let rec Numdel n num = 
+//Метод 2 - найти количество нечетных цифр числа, больших 3
+let col_vo n = 
+    let rec Num n num = 
         if n = 0 then num
         else 
             let nextNum = 
                 if (n%10)%2=1 && (n%10)>3 then (num+1)
                 else num
             let NextN = n/10
-            Numdel NextN nextNum
-    Numdel n 0
+            Num NextN nextNum
+    Num n 0
 
+//Метод 3 - найти произведение таких делителей числа, сумма цифр которых меньше сумма цифр исходного числа
+let Sumn n = 
+    let rec Sum1 n init = 
+        if n = 0 then init
+        else 
+            let nextInit = init+(n%10)
+            let nextN=n/10
+            Sum1 nextN nextInit
+    Sum1 n 0
 
-let  vod = 
+let Div n = 
+    let rec ProductDiv n result div = 
+        if div = 1 then result
+        else 
+            let nextRes= 
+                if n%div=0 && (Sumn div < Sumn n) then result*div
+                else result
+            let nextDiv = div-1
+            ProductDiv n nextRes nextDiv
+    ProductDiv n 1 n
+
+[<EntryPoint>]
+let main argv =
     System.Console.WriteLine("Введите число:")
     let n = System.Convert.ToInt32(System.Console.ReadLine())
-    let x = sum' n
-    Console.WriteLine(x)
-    let y =num n
-    Console.WriteLine(y)
-    0
-
-
+    let res1 = Sum n
+    Console.WriteLine("Сумма простых делителей числа = {0}",res1)
+    let res2=col_vo n
+    Console.WriteLine("Количество нечетных цифр числа, которые больше 3  = {0}",res2)
+    let res3=Div n
+    Console.WriteLine("Произведение делителей числа, сумма цифр которых меньше суммы цифр исходного числа = {0}",res3)
+    0 
