16bc254f0b7a9c4763ab9caef4d7d9f24c7b9fbb
Dementr2302 <94387603+Dementr2302@users.noreply.github.com>
2022-05-23 23:22:17 +0300

    18

---
+++ LB_7_Num_8/Program.fs
@@ -0,0 +1,24 @@
+﻿open System
+
+let readArray n =
+    let rec readarray n arr = 
+        match n with 
+        |0 ->arr
+        |_->
+            let newEl=Console.ReadLine()|>Convert.ToInt32
+            let newArr = Array.append arr [|newEl|]
+            readarray (n-1) newArr
+    readarray n [||]
+
+
+let writeArray arr = 
+    printfn "%A" arr
+
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Введите размерность массива,затем элементы массива:")
+    let newArray= Console.ReadLine() |> Convert.ToInt32|>readArray
+    let ResultArray = Array.rev newArray
+    Console.WriteLine("Результирующий массив:")
+    writeArray ResultArray
+    0
