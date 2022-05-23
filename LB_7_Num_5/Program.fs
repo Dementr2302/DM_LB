b9ed338793d18d8bfe94515fe864d42e8ac6ddef
Dementr2302 <94387603+Dementr2302@users.noreply.github.com>
2022-05-23 22:17:58 +0300

    41

---
+++ LB_7_Num_5/Program.fs
@@ -0,0 +1,27 @@
+﻿open System
+
+let rec readList n =
+    Console.WriteLine("Введите список: ")
+    List.init(n) (fun index->Console.ReadLine()|>Convert.ToDouble)
+
+let readData = 
+    Console.WriteLine("Введите размерность списка:  ")
+    let n=System.Convert.ToInt32(System.Console.ReadLine())
+    readList n
+
+let rec writeList = function
+    [] ->   let z = System.Console.ReadKey()
+            0
+    | (head : int)::tail -> 
+                       System.Console.WriteLine(head)
+                       writeList tail
+
+
+
+let F1 list = List.average (List.map (fun (x:float)-> Math.Abs(x)) list )
+   
+[<EntryPoint>]
+let main argv =
+   Console.WriteLine("Cреднее арифметическое модулей его элементов:")
+   readData|> F1 |> Console.WriteLine;
+   0 
