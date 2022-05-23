42b5d428f1e36c8e18f5c397faeb1afa898b5eb1
Dementr2302 <94387603+Dementr2302@users.noreply.github.com>
2022-05-23 17:04:50 +0300

    Add files via upload

---
+++ LB_7_Num_1/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+
+let rec readList n =
+    List.init(n) (fun index->Console.ReadLine()|>Int32.Parse)
+
+let readData() = 
+    let n=System.Convert.ToInt32(System.Console.ReadLine())
+    readList n
+
+let rec writelist list=
+    List.iter(fun x->printfn "%O" x) list
+
+
+
+
+let IndMax list = List.length list - (List.findIndexBack (fun x-> x=(List.max list) ) list)-1 
+
+[<EntryPoint>]
+let main argv =
+   readData() |> IndMax |>Console.WriteLine;
+    0
