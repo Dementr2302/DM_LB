141bcbabe64deb7f2e1834785a397d93b998f335
Dementr2302 <94387603+Dementr2302@users.noreply.github.com>
2022-05-24 09:18:27 +0300

    51

---
+++ LB_7_Num_6/Program.fs
@@ -0,0 +1,42 @@
+﻿open System
+
+let rec readList n = 
+    if n=0 then []
+    else
+    let Head = System.Convert.ToInt32(System.Console.ReadLine())
+    let Tail = readList (n-1)
+    Head::Tail
+
+let readData = 
+    Console.WriteLine("Введите размерность списка:  ")
+    let n=System.Convert.ToInt32(System.Console.ReadLine())
+    Console.WriteLine("Введите список: ")
+    readList n
+
+let rec writeList = function
+    [] ->   let z = System.Console.ReadKey()
+            0
+    | (head : int)::tail -> 
+                       System.Console.WriteLine(head)
+                       writeList tail
+
+
+
+let F1 list = 
+  List.fold (
+        fun m x -> 
+            let f = fst(m) @ [fst(x)]
+            let s = snd(m) @ [snd(x)]
+            (f,s))
+        ([], [])(List.countBy (fun x->x) list)
+    
+
+[<EntryPoint>]
+let main argv =
+    let newList = readData|> F1
+    Console.WriteLine("Список L1:")
+    writeList (fst(newList))
+    Console.WriteLine("Список L2:")
+    writeList (snd(newList))
+    
+    0 
